597baf6767df1bebc57b916dc04742c42138e8df
Atul Soman <atul.soman@microfocus.com>
2017-03-23 16:36:09 +0530

    Initial commit

---
+++ TestCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> testDictionary = new Dictionary<string, string>();
+            testDictionary.Add("1", "one");
+            if (testDictionary.ContainsKey("1"))
+            {
+                Debug.WriteLine("Already contains key 1");
+            }
+            else
+            {
+                testDictionary.Add("1", "ONE");
+            }
+
+            Debug.WriteLine(testDictionary);
+
+            String key = String.Format("{0}:{1}", "key", "value");
+            Debug.WriteLine(key);
+        }
+    }
+}
